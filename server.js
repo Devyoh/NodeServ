4492eb5e62e40479ffc1f3dafe41c6b7a092214c
Devyoh <yoh-prodev@outlook.fr>
2023-05-29 17:30:08 +0200

    add doc on server.js

--- server.js
+++ server.js
@@ -1,28 +1,32 @@
 const express = require('express')
 const path = require('path')
 
+//permet l'utilisation de ".env" avec des variables d'environnement.
 require('dotenv').config()
 
-
+//Définit un port de base OU 7000 s'il n'est pas reconnu.
 const PORT = process.env.PORT || 7000
 
 const app = express()
 
-//Use API
+//Utilisation de l'API
 app.use(express.json())
 
 app.use(express.static('client/build'))
 
+//Récupération d'une API factice.
 app.get('/api/youtube', (_ , res) => {
     res.send({
         msg: 'Bonjour les gens'
     })
 })
 
+//permet l'affichage de toutes les pages retournées. **__dirname** = racine dynamique.
 app.get('/*', (_ , res) => {
     res.sendFile(path.join(__dirname, './client/build/index.html'))
 })
 
+//Message de confirmation du port utilisé lors du lancement du serveur.
 app.listen(PORT, () => {
     console.log(`Le serveur est lancé sur le port : ${PORT}`)
 })
